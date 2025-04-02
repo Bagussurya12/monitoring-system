a82c7d54bec2f259a35e1c478c572a679aedf210
Bagussurya12 <bagus.suryaindrajati@gmail.com>
2025-04-02 14:15:50 +0700

    add pages master-data/countries

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundModule } from './pages/Auth/not-found/not-found.module';
 import { SettingsLinkModule } from './components/sidebar-menu/links/settings-link/settings-link/settings-link.module';
+import { CountriesComponent } from './pages/master-data/countries/countries.component';
+import { CreateComponent } from './pages/master-data/countries/create/create.component';
+import { ViewComponent } from './pages/master-data/countries/view/view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CountriesComponent,
+    CreateComponent,
+    ViewComponent,
 
   ],
   imports: [
